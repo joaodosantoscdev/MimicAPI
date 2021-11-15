bd09d46c2afddbca926c14fd2551f7cf92b3bb3a
João Vítor Carvalho <mistingerr@gmail.com>
2021-11-15 17:30:37 -0300

    api-database

--- MimicAPI/Models/Word.cs
+++ MimicAPI/Models/Word.cs
@@ -7,5 +7,10 @@ namespace MimicAPI.Models
 {
     public class Word
     {
+        public int  Id { get; set; }
+        public string Name { get; set; }
+        public int Score { get; set; }
+        public bool Active { get; set; }
+        public DateTime? Att { get; set; }
     }
 }
