dc3ca23e2b163bb3da912ce2981ef8255b92fd84
João Vítor Carvalho <mistingerr@gmail.com>
2021-11-15 18:15:00 -0300

    added-controller

--- MimicAPI/Controllers/WordController.cs
+++ MimicAPI/Controllers/WordController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MimicAPI.Database;
+using MimicAPI.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,11 +8,49 @@ using System.Threading.Tasks;
 
 namespace MimicAPI.Controllers
 {
-    public class WordController : Controller
+    public class PalavrasController : ControllerBase
     {
-        public IActionResult Index()
+    
+        private readonly MimicContext _context;
+        public PalavrasController(MimicContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        //APP
+        public ActionResult GetAll()
+        {
+            return Ok(_context.Words);
+        }
+
+
+        //WEB
+        public ActionResult GetById(int id)
+        {
+            return Ok(_context.Words.Find(id));
+        }
+
+        public ActionResult Add(Word word)
+        {
+            _context.Words.Add(word);
+
+            return Ok();
+        }
+
+        public ActionResult Update(int id, Word word)
+        {
+            _context.Words.Update(word);
+
+            return Ok();
+        }
+
+        public ActionResult Delete(int id)
+        {
+            _context.Words.Remove(_context.Words.Find(id));
+
+            return Ok();
         }
     }
 }
+
+
