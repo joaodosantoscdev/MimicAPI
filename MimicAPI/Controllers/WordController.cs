d2101385a5afc4d4b3b2c603073a0d51b5b206d7
João Vítor Carvalho <mistingerr@gmail.com>
2021-11-17 17:45:07 -0300

    att

--- MimicAPI/Controllers/WordController.cs
+++ MimicAPI/Controllers/WordController.cs
@@ -5,9 +5,7 @@ using MimicAPI.Helpers;
 using MimicAPI.Models;
 using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace MimicAPI.Controllers
 {
