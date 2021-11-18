5498ba89cc8682c38176bcfc79bd92709bbee89d
João Vítor Carvalho <mistingerr@gmail.com>
2021-11-18 19:52:33 -0300

    api validation

--- MimicAPI/Models/Word.cs
+++ MimicAPI/Models/Word.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,10 @@ namespace MimicAPI.Models
 {
     public class Word
     {
-        public int  Id { get; set; }
+        public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public int Score { get; set; }
         public bool Active { get; set; }
         public DateTime Created { get; set; }
